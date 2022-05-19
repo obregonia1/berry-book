f71e337286034d2cf69721cab7b46e772723c090
Kentaro Takeyama <[n.y.skate.of.minds@gmail.com](mailto:n.y.skate.of.minds@gmail.com)>
2022-05-19 18:05:35 +0900

    update src/ex4.6.3.ts

--- src/ex4.6.3.ts
+++ src/ex4.6.3.ts
@@ -1,11 +1,11 @@
-function map(array: number[], callback: (num: number) => number): number[] {
-  const res: number[] = [];
+function map<T, U>(array: T[], callback: (num: T) => U): U[] {
+  const res: U[] = [];
   for (const i of array) {
     res.push(callback(i))
   }
   return res;
 }
 
-const data = [1, 1, 2, 3, 5, 8, 13];
-const result = map(data, (x) => x * 10);
+const data = [1, -3, -2, 8, 0, -1];
+const result: boolean[] = map(data, (x) => x >= 0);
 console.log(result);
